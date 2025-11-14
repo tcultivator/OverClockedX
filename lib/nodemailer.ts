d2b75c92c52b1a925d444e6b8c21af449e3dae7b
tcultivator <whypanahon06@gmail.com>
2025-11-14 11:18:05 +0800

    Update nodemailer.ts

--- lib/nodemailer.ts
+++ lib/nodemailer.ts
@@ -1,32 +1,25 @@
-import nodemailer from 'nodemailer';
-
+import nodemailer from 'nodemailer'
 type sendMailProps = {
     to: string;
     sub: string;
     message: string;
-};
-
+}
 const transporter = nodemailer.createTransport({
-    host: process.env.NODEMAILER_HOST, 
-    port: 587,
-    secure: false,                       
+    secure: true,
+    host: process.env.NODEMAILER_HOST,
+    port: 465,
     auth: {
         user: process.env.NODEMAILER_USER,
         pass: process.env.NODEMAILER_PASS
     }
-});
+})
 
-export async function sendMail({ to, sub, message }: sendMailProps) {
-    try {
-        const info = await transporter.sendMail({
-            from: process.env.NODEMAILER_USER, 
-            to,
-            subject: sub,
-            html: message
-        });
 
-        console.log('Email sent:', info.messageId);
-    } catch (err) {
-        console.error('Email send error:', err);
-    }
+export function sendMail({ to, sub, message }: sendMailProps) {
+    transporter.sendMail({
+        to: to,
+        subject: sub,
+        html: message
+    })
+    console.log('message Sent')
 }
