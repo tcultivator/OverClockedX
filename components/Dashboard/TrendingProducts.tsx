4bd0b17267b7505675d402dfadf86cd0fc603e16
tomatoCultivator <lpanahon06@gmail.com>
2025-11-20 03:30:27 +0800

    update(Promotions): added a reusable UI for promotions, each promotion type has different UI base on promotion types, i also use tailwind container queries for responsiveness of UI in grid,i also added a simple animation using framer motion, this animation will trigger if the promotions array shuffle, using that shuffle i dont need to display all promotion in that components, i just need to display 5 promotions, then shuffle the display, so if the shuffle is trigger, it trigger the simple animation using framer motion, that promotion UI is not an image, it has some navigation like redirect to product preview, thats why it need custome UI that look like a promotion Images, and that also why i need to use tailwind container queries

--- components/Dashboard/TrendingProducts.tsx
+++ components/Dashboard/TrendingProducts.tsx
@@ -1,20 +1,10 @@
 "use client"
 import React from 'react';
-import Image from 'next/image';
 import { useEffect, useState } from 'react';
-import { Label } from '../ui/label';
-import { Button } from '../ui/button';
-type promotion_products = {
-    product_id: string;
-    product_name: string;
-    product_image: string;
-    brand: string;
-    price: number;
-    start_date: Date;
-    end_date: Date;
-    value: number;
-    promotion_type: string;
-}
+import { promotion_products } from '@/types/promotion_products_types';
+import DiscountedProductsUI from '@/utils/PromotionsTypeUI/DiscountedProductsUI';
+import FlashSaleUI from '@/utils/PromotionsTypeUI/FlashSaleUI';
+import { motion, AnimatePresence } from 'framer-motion'
 const TrendingProducts = () => {
     const [promotionProducts, setPromotionProducts] = useState<promotion_products[]>([])
     const images: string[] = [
@@ -23,8 +13,29 @@ const TrendingProducts = () => {
         "/promotions/pm3.jpg",
         "/promotions/pm4.jpg",
         "/promotions/pm5.jpg",
+        "/promotions/test.png"
     ];
 
+    function shuffleArray<T>(array: T[]): T[] {
+        const shuffled = [...array];
+        for (let i = shuffled.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+        }
+        return shuffled;
+    }
+    useEffect(() => {
+
+        const interval = setInterval(() => {
+            if (promotionProducts.length > 0) {
+                setPromotionProducts(prev => shuffleArray(prev));
+            }
+        }, 8000);
+
+        return () => clearInterval(interval);
+
+
+    }, []);
     useEffect(() => {
         const fetProductsPromotion = async () => {
             try {
@@ -46,62 +57,45 @@ const TrendingProducts = () => {
     }, [])
 
     return (
-        <section className="p-4 m-auto px-[2px] md:px-[5%]">
+        <section className="p-4 m-auto px-[2px] xl:px-[5%]">
             <div className="w-full">
                 <h2 className="text-2xl font-anton text-[40px] text-left  w-max p-2 text-white rounded-t-md">Promotions</h2>
             </div>
-            {promotionProducts.length > 0 && < div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                {/* Left side */}
-                <div className="grid grid-rows-1 md:grid-rows-2 lg:grid-rows-1 gap-4">
-                    <div className='bg-white/10 rounded-md p-2 flex flex-col justify-between text-white'>
-                        <div className='flex justify-between items-center'>
-                            <div className='flex gap-1 items-center'>
-                                <Label>November</Label>
-                                <Label>{promotionProducts[0].promotion_type}</Label>
-                            </div>
-                            <div className='p-2 bg-red-400 flex items-center justify-center text-white'>
-                                <Label>₱{promotionProducts[0].value}<span>OFF</span></Label>
-                            </div>
-                        </div>
-                        <div className='flex gap-1 items-center'>
-                            <div>
-                                <Image
-                                    src={promotionProducts[0].product_image}
-                                    alt="Trending Product 2"
-                                    className="w-full h-full rounded-md md:block"
-                                    width={800}
-                                    height={800}
-                                />
-                            </div>
+            <AnimatePresence mode='wait'>
+                {promotionProducts.length > 0 && < div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    {/* Left side */}
+                    <div className="grid grid-rows-1 md:grid-rows-2 lg:grid-rows-1 gap-4">
+                        <motion.div className="w-full h-full object-cover rounded-md" key={promotionProducts[0].product_id}
+                            initial={{ opacity: 0, transitionDuration: .5, scale: 0.9 }}
+                            animate={{ opacity: 1, transitionDuration: .5, scale: 1 }}
+                            exit={{ opacity: 0, transitionDuration: .5, scale: 0.9 }}
+                            transition={{ duration: .2 }}>
+                            {
+                                promotionProducts[0].promotion_type == 'FlashSale' ?
+                                    <FlashSaleUI promotionProducts={promotionProducts[0]} /> :
+                                    <DiscountedProductsUI promotionProducts={promotionProducts[0]} />
+                            }
 
-                            <div>
-                                <Label>{promotionProducts[0].brand}</Label>
-                                <Label>{promotionProducts[0].product_name}</Label>
-                                <Label>{new Intl.NumberFormat('en-PH', {
-                                    style: 'currency',
-                                    currency: 'PHP',
-                                }).format(promotionProducts[0].price)}</Label>
+                        </motion.div>
 
-                            </div>
-                        </div>
-                        <div className='flex flex-col items-center justify-center gap-2'>
-                            <Button variant={'secondary'}>Order Now</Button>
-                            <Label className='p-1 border-y-[2px] border-white/50 rounded'>https://overclockedx.onrender.com/</Label>
-                        </div>
+                        {/* On medium screens, show second image on left */}
+                        <motion.div className="w-full h-full object-cover rounded-md md:block lg:hidden" key={promotionProducts[1].product_id}
+                            initial={{ opacity: 0, transitionDuration: .5, scale: 0.9 }}
+                            animate={{ opacity: 1, transitionDuration: .5, scale: 1 }}
+                            exit={{ opacity: 0, transitionDuration: .5, scale: 0.9 }}
+                            transition={{ duration: .2 }}>
+                            {
+                                promotionProducts[1].promotion_type == 'FlashSale' ?
+                                    <FlashSaleUI promotionProducts={promotionProducts[1]} /> :
+                                    <DiscountedProductsUI promotionProducts={promotionProducts[1]} />
+                            }
+
+                        </motion.div>
+
                     </div>
 
-                    {/* On medium screens, show second image on left */}
-                    <Image
-                        src={images[1]}
-                        alt="Trending Product 2"
-                        className="w-full h-full object-cover rounded-md md:block lg:hidden"
-                        width={800}
-                        height={800}
-                    />
-                </div>
-
-                {/* Right side */}
-                <div className="
+                    {/* Right side */}
+                    <div className="
                     grid 
                     grid-cols-1 
                     md:grid-cols-1 
@@ -110,28 +104,45 @@ const TrendingProducts = () => {
                     lg:grid-rows-2 
                     gap-4
                 ">
-                    {/* On medium: show images[2,3,4]; On large: images[1,2,3,4] */}
-                    {images.slice(2, 5).map((img, idx) => (
-                        <Image
-                            key={idx}
-                            src={img}
-                            alt={`Trending Product ${idx + 3}`}
-                            className="w-full h-full object-cover rounded-md"
-                            width={800}
-                            height={800}
-                        />
-                    ))}
+                        {/* On medium: show images[2,3,4]; On large: images[1,2,3,4] */}
+                        {promotionProducts.slice(2, 5).map((data, idx) => (
 
-                    {/* Only show images[1] on large, since it was already shown on md */}
-                    <Image
-                        src={images[1]}
-                        alt="Trending Product 2"
-                        className="w-full h-full object-cover rounded-md hidden lg:block"
-                        width={800}
-                        height={800}
-                    />
-                </div>
-            </div>}
+                            <motion.div className="w-full h-full object-cover rounded-md"
+                                key={data.product_id}
+                                initial={{ opacity: 0, transitionDuration: .5, scale: 0.9 }}
+                                animate={{ opacity: 1, transitionDuration: .5, scale: 1 }}
+                                exit={{ opacity: 0, transitionDuration: .5, scale: 0.9 }}
+                                transition={{ duration: .2 }}
+                                
+                                >
+                                {
+                                    data.promotion_type == 'FlashSale' ?
+                                        <FlashSaleUI promotionProducts={data} /> :
+                                        <DiscountedProductsUI promotionProducts={data} />
+                                }
+
+                            </motion.div>
+
+
+                        ))}
+
+                        {/* Only show images[1] on large, since it was already shown on md */}
+                        <motion.div className="w-full h-full object-cover rounded-md hidden lg:block" key={promotionProducts[1].product_id}
+                            initial={{ opacity: 0, transitionDuration: .5, scale: 0.9 }}
+                            animate={{ opacity: 1, transitionDuration: .5, scale: 1 }}
+                            exit={{ opacity: 0, transitionDuration: .5, scale: 0.9 }}
+                            transition={{ duration: .2 }}>
+                            {
+                                promotionProducts[1].promotion_type == 'FlashSale' ?
+                                    <FlashSaleUI promotionProducts={promotionProducts[1]} /> :
+                                    <DiscountedProductsUI promotionProducts={promotionProducts[1]} />
+                            }
+
+                        </motion.div>
+
+                    </div>
+                </div>}
+            </AnimatePresence>
         </section >
     );
 };
---
+++ types/promotion_products_types.ts
@@ -0,0 +1,11 @@
+export type promotion_products = {
+    product_id: string;
+    product_name: string;
+    product_image: string;
+    brand: string;
+    price: number;
+    start_date: Date;
+    end_date: Date;
+    value: number;
+    promotion_type: string;
+}
